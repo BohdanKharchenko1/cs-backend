3721ac7ed4b26470c3ea7abf74bb4bd1dce5988e
Bohdan <84513013+naughtyCURSED@users.noreply.github.com>
2025-10-15 22:28:07 +0200

    stuff

---
+++ src/database/database.datasource.ts
@@ -0,0 +1,32 @@
+import { DataSource } from 'typeorm';
+import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
+import { glob } from 'glob';
+import * as dotenv from 'dotenv';
+
+const envFile =
+  process.env.NODE_ENV === 'production'
+    ? '.production.env'
+    : '.development.env';
+dotenv.config({ path: envFile });
+
+const configService = new ConfigService();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: configService.get<string>('POSTGRES_HOST'),
+  port: configService.get<number>('POSTGRES_PORT'),
+  username: configService.get<string>('POSTGRES_USER'),
+  password: configService.get<string>('POSTGRES_PASSWORD'),
+  database: configService.get<string>('POSTGRES_DB'),
+  url: configService.get<string>('DATABASE_URL'),
+  entities: glob.sync(join(__dirname, '..', '..', '**', '*.entity.{ts,js}')),
+  migrations: [join(__dirname, 'migrations', '*{.ts,.js}')],
+  ssl:
+    configService.get<string>('NODE_ENV') === 'local'
+      ? false
+      : { rejectUnauthorized: false },
+  migrationsTableName: 'migrations',
+  synchronize: false,
+  logging: false,
+});
